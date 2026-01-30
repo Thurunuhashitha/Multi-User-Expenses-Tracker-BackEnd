b1c677793cf9edc4ff68f4d2a4d7f028c6318309
Thurunu Hashitha <106402728+Thurunuhashitha@users.noreply.github.com>
2026-01-30 17:02:03 +0530

    add CORS (HTTP-header based security mechanism)

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express'); 
+const cors = require('cors');
 
 const app = express();
+
+// 🔥 VERY IMPORTANT
+app.use(cors()); // allow all origins
  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
